6ec084d150c43e275ee777e632e9d07f30872ac3
Jose-program-html <josey_8@hotmail.com>
2015-08-04 09:35:55 -0500

    Home

--- app/src/main/java/shary/recetas/activity/HomeFragment.java
+++ app/src/main/java/shary/recetas/activity/HomeFragment.java
@@ -30,15 +30,46 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-        VideoView videoView = (VideoView) rootView.findViewById(R.id.videoView);
+        VideoView videoView = (VideoView) rootView.findViewById(R.id.videoView1);
         try {
             MediaController mediaController = new MediaController(this.getActivity());
             mediaController.setAnchorView(videoView);
             Uri uri = Uri.parse(getString(R.string.api_endpoint)+"/assets/video/video.mp4");
             videoView.setMediaController(mediaController);
             videoView.setVideoURI(uri);
-            videoView.start();
-
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        VideoView videoView2 = (VideoView) rootView.findViewById(R.id.videoView2);
+        try {
+            MediaController mediaController = new MediaController(this.getActivity());
+            mediaController.setAnchorView(videoView2);
+            Uri uri = Uri.parse(getString(R.string.api_endpoint)+"/assets/video/video2.mp4");
+            videoView2.setMediaController(mediaController);
+            videoView2.setVideoURI(uri);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        VideoView videoView3 = (VideoView) rootView.findViewById(R.id.videoView3);
+        try {
+            MediaController mediaController = new MediaController(this.getActivity());
+            mediaController.setAnchorView(videoView3);
+            Uri uri = Uri.parse(getString(R.string.api_endpoint)+"/assets/video/video3.mp4");
+            videoView3.setMediaController(mediaController);
+            videoView3.setVideoURI(uri);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        VideoView videoView4 = (VideoView) rootView.findViewById(R.id.videoView4);
+        try {
+            MediaController mediaController = new MediaController(this.getActivity());
+            mediaController.setAnchorView(videoView4);
+            Uri uri = Uri.parse(getString(R.string.api_endpoint)+"/assets/video/video4.mp4");
+            videoView4.setMediaController(mediaController);
+            videoView4.setVideoURI(uri);
         }catch (Exception e)
         {
             e.printStackTrace();
